768c1b66bfdabdef6d62c3f29a1b08ffdb7a0237
jitaprov <jitka.provaznikova@gymkren.cz>
2025-12-03 13:47:19 +0100

    matrix

--- matrix.py
+++ matrix.py
@@ -7,7 +7,7 @@ class Matrix:
     """
  def __init__(self, matrix):
         self.matrix = matrix
-        
+
     """
     10 bodů + max 10 bonusových bodů za hezkost
     TODO:
@@ -30,4 +30,24 @@ class Matrix:
     ————— ————— —————
     """
     def print(self):
-        pass
+        length = 0
+        for row in self.matrix:
+            length = max(max(row), length)
+        print(length)
+        print(len(self.matrix)* (" " + "—" * len(str(length))))
+        for row in self.matrix:
+            print("|", end="")
+            for column in row:
+                print(" " * (len(str(length)) - len(str(column))), column, sep="", end="|")
+            print()
+            print(len(self.matrix)* (" " + "—" * len(str(length))))
+     
+    
+    def __repr__(self):
+       return str(self.matrix)
+            
+            
+            
+if __name__ == "__main__":
+    matice1 = Matrix([[1, 25436, 3], [4, 5, 74656], [7, 8, 9]])
+    matice1.print()
