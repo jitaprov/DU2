23bf99218c8da163e6b88975d20dfa1d4e1de9bc
jitaprov <jitka.provaznikova@gymkren.cz>
2025-12-03 14:01:49 +0100

    mfact

--- matrix_factory.py
+++ matrix_factory.py
@@ -8,7 +8,9 @@ class MatrixFactory:
     10 bodů
     TODO: Inicializujte MatrixFactory. Jaké atributy bude potřebovat továrna na matice? Potřebuje nějaké vystavovat?
     """
-
+    def __init__(self, size):
+        self._size = size
+        self._storage = []
     """
     10 bodů
     TODO:
@@ -16,7 +18,14 @@ class MatrixFactory:
     Novou matici potom přidá do skladu matic. Náhodný prvek dostanete pomocí random.radint(dolni_hranice, horni_hranice)
     """
     def create_random_matrix(self):
-        pass
+        matrix = []
+        for _ in range(self._size):
+            row = []
+            for _ in range(self._size):
+                row.append(random.randint(0, 9))
+            matrix.append(row)
+        matrix = Matrix(matrix)
+        self._storage.append(matrix)
 
     """
     5 bodů
@@ -42,8 +51,13 @@ class MatrixFactory:
     Když je seznam matic prázdný, tak vypíše, že žádná továrna žádnou matici nemá.
     """
     def print_all_matrices(self):
-        pass
-
+        if self._storage == []:
+            print("němame")
+            return
+        for i in range(len(self._storage)):
+            print("Index", i)
+            self._storage[i].print()
+            print()
     """
     5 bodů
     TODO:
@@ -51,8 +65,11 @@ class MatrixFactory:
     Když je seznam matic prázdný, tak vypíše, že žádná továrna žádnou matici nemá.
     """
     def print_last(self):
-        pass
-
+        if self._storage == []:
+            print("němame")
+            return
+        print("last matrix")
+        self._storage[-1].print()
 
     """
     15 bodů
@@ -62,12 +79,19 @@ class MatrixFactory:
     Můžete předpokládat, že matice na indexech existují nebo vypsat hlášku, že matice na takových indexech nejsou
     """
     def create_by_sum(self, first_matrix_index, second_matrix_index):
-        pass
+        if first_matrix_index >= len(self._storage) or second_matrix_index >= len(self._storage):
+            print("němame")
+            return
+        sum_matrix = []
+        for i in range(self._size):
+            row = []
+            for j in range(self._size):
+                row.append(self._storage[first_matrix_index].matrix[i][j] + self._storage[second_matrix_index].matrix[i][j])
+            sum_matrix.append(row)
+            
+        sum_matrix = Matrix(sum_matrix)
+        self._storage.append(sum_matrix)
 
-
-    # Můžete a nemusíte využít, pomocná metoda pro create_by_multiplication. Případně smažte
-    def _multiplication_partial_sum(self, first_matrix, second_matrix, first_matrix_row, second_matrix_col):
-        pass
 
     """
     15 bodů
@@ -79,4 +103,30 @@ class MatrixFactory:
     Doporučuji využít separátní funkci, která bude dostane řádek první matice a sloupec druhé matice a vrátí prvek, který vznikne součtem součinů.
     """
     def create_by_multiplication(self, first_matrix_index, second_matrix_index):
-        pass
+        if first_matrix_index >= len(self._storage) or second_matrix_index >= len(self._storage):
+            print("němame")
+            return
+        mul_matrix = []
+        matrix1 = self._storage[first_matrix_index].matrix
+        matrix2 = self._storage[second_matrix_index].matrix
+        for i in range(self._size):
+            row = []
+            for j in range(self._size):
+                number = 0
+                for k in range(self._size):
+                    number += matrix1[i][k] * matrix2[k][j]
+                row.append(number)
+            mul_matrix.append(row)
+        mul_matrix = Matrix(mul_matrix)
+        self._storage.append(mul_matrix)
+
+if __name__ == "__main__":
+    tovarna = MatrixFactory(2)
+    
+    tovarna._storage.append(Matrix([[1,2],[3,4]]))
+    tovarna._storage.append(Matrix([[5,6],[7,8]]))
+    tovarna.create_by_multiplication(0,1)
+    
+    
+    print(tovarna._storage)
+    tovarna.print_all_matrices()
